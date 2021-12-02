f268e3f07df947dd443986c10562ee21c80217fd
Ricardo Chavez <ricardo.chavez.t@gmail.com>
2021-12-01 19:24:03 -0500

    Day 1 part 2

--- day1.hs
+++ day1.hs
@@ -5,8 +5,16 @@ countIncreases (x:xs) =
   let count = if head xs > x then 1 else 0
   in count + countIncreases xs
 
+sum3 :: [Int] -> [Int]
+sum3 xs
+  | size < 3  = []
+  | otherwise = [sum (take 3 xs)] ++ sum3 (tail xs)
+  where size = length xs
+
 main = do
   input <- getContents
   let inputData = map read $ lines input
   let result = countIncreases inputData
   print result
+  let result2 = countIncreases (sum3 inputData)
+  print result2
