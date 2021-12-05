d769065cdebd4bb717b286cbd8c26dbb0a0d5495
Ricardo Chavez <ricardo.chavez.t@gmail.com>
2021-12-04 23:44:42 -0500

    Day 3 (refactored)

--- day3.hs
+++ day3.hs
@@ -1,46 +1,32 @@
 import Data.List
 import Data.Char
 
-parseLine :: String -> (Int, Int)
-parseLine line = foldl (\(z, o) x -> if x == '0' then (z+1, o) else (z, o+1)) (0,0) line
-
-sumLines :: [String] -> [(Int, Int)]
-sumLines inputLines = map parseLine $ transpose inputLines
-
 parseBits :: [Int] -> Int
 parseBits bits = foldl (\acc b -> acc*2+b) 0 bits
 
 parseBitString :: String -> Int
 parseBitString bitString = parseBits $ map digitToInt bitString
 
-filterInputs :: [String] -> Int -> Char -> [String]
-filterInputs [] _ _ = []
-filterInputs [x] _ _ = [x]
-filterInputs inputs i c = filter (\input -> (input !! i) == c) inputs
+parseLine :: String -> (Int, Int)
+parseLine line = foldl (\(z, o) x -> if x == '0' then (z+1, o) else (z, o+1)) (0,0) line
 
-filterInputs' :: [String] -> Int -> [String]
-filterInputs' inputs i =
+sumLines :: [String] -> [(Int, Int)]
+sumLines inputLines = map parseLine $ transpose inputLines
+
+filterInputs :: ((Int,Int) -> Char) -> [String] -> Int -> [String]
+filterInputs f inputs i =
   let reportSum = sumLines inputs
-  in filterInputs inputs i (let (z,o) = reportSum !! i in (if z > o then '0' else '1'))
+  in filter (\input -> (input !! i) == (f $ reportSum !! i)) inputs
 
-filterInputsRecursive :: [String] -> Int -> [String]
-filterInputsRecursive [] _ = []
-filterInputsRecursive [x] _ = [x]
-filterInputsRecursive inputs i =
-  let nextInputs = filterInputs' inputs i
-  in filterInputsRecursive nextInputs (i+1)
+filterInputsRecursive :: Int -> [String] -> ([String] -> Int -> [String]) -> [String]
+filterInputsRecursive _ [] _ = []
+filterInputsRecursive _ [x] _ = [x]
+filterInputsRecursive i inputs f =
+  let nextInputs = f inputs i
+  in filterInputsRecursive (i+1) nextInputs f
 
-filterInputs'' :: [String] -> Int -> [String]
-filterInputs'' inputs i =
-  let reportSum = sumLines inputs
-  in filterInputs inputs i (let (z,o) = reportSum !! i in (if z <= o then '0' else '1'))
-
-filterInputsRecursive' :: [String] -> Int -> [String]
-filterInputsRecursive' [] _ = []
-filterInputsRecursive' [x] _ = [x]
-filterInputsRecursive' inputs i =
-  let nextInputs = filterInputs'' inputs i
-  in filterInputsRecursive' nextInputs (i+1)
+getRating :: [String] -> ([String] -> Int -> [String]) -> [String]
+getRating = filterInputsRecursive 0
 
 main = do
   input <- getContents
@@ -51,8 +37,10 @@ main = do
   putStrLn $ "gamma rate: " ++ (show gammaRate) ++ ", epsilon rate: " ++ (show epsilonRate)
   putStrLn $ "power consumption: " ++ (show (gammaRate * epsilonRate))
 
-  let o2GenRating = parseBitString $ head $ filterInputsRecursive inputLines 0
-  let co2ScrubRating = parseBitString $ head $ filterInputsRecursive' inputLines 0
+  let o2GenRating = parseBitString $ head $
+                    getRating inputLines (filterInputs (\(z,o) -> if z > o then '0' else '1'))
+  let co2ScrubRating = parseBitString $ head $
+                       getRating inputLines (filterInputs (\(z,o) -> if z <= o then '0' else '1'))
 
   putStrLn $ "oxygen generator rating: " ++ (show o2GenRating) ++ ", CO2 scrubber rating: " ++ (show co2ScrubRating)
   putStrLn  $ "life support rating: " ++ (show (o2GenRating * co2ScrubRating))
