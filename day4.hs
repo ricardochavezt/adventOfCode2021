cbbaadf99b9e0956f09c37a56d700d731b44d847
Ricardo Chavez <ricardo.chavez.t@gmail.com>
2021-12-07 00:31:48 -0500

    Day 4, part 1

---
+++ day4.hs
@@ -0,0 +1,28 @@
+import Data.List
+import Data.Function
+
+splitOn :: (Eq a) => a -> [a] -> [[a]]
+splitOn x ys = filter (not . any (== x)) . groupBy ((==) `on` (== x)) $ ys
+
+checkNumbers :: (Eq a) => [a] -> [[a]] -> Bool
+checkNumbers l b = case result of Just _ -> True
+                                  Nothing -> False
+  where result = find (\x -> all (`elem` l) x) b
+
+checkAllBoardsStep :: (Eq a) => [[[a]]] -> (Maybe [[a]], [a]) -> [a] -> (Maybe [[a]], [a])
+checkAllBoardsStep _ all@(Just x, l) _ = all
+checkAllBoardsStep bs (acc, _) l = ((find (checkNumbers l) bs), l)
+
+sumUnmarked :: (Num a, Eq a) => Maybe [[a]] -> [a] -> a
+sumUnmarked Nothing _ = 0
+sumUnmarked (Just wb) wn = sum $ concatMap (filter (\x -> not (x `elem` wn))) wb
+
+main = do
+  input <- getContents
+  let (numbers:boardLines) = lines input
+  let numberList = map read $ splitOn ',' numbers :: [Int]
+  let boards = map (\xs -> map (\y -> map read (words y)) xs) $ splitOn "" boardLines :: [[[Int]]]
+
+  let (winnerBoard, winnerNums) = foldl (checkAllBoardsStep boards) (Nothing, []) $ inits numberList
+
+  print $ (sumUnmarked winnerBoard winnerNums) * (last winnerNums)
